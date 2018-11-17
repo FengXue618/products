d88493905c412aa615de38050c2d42b875a415d4
Feng Xue <frankxue6182@outlook.com>
2018-11-17 11:43:57 +0000

    refactor

--- products.py
+++ products.py
@@ -1,46 +1,48 @@
 
 import os # operating system (操作系统)
 # 读取档案
-products = []
-if os.path.isfile('products.csv'): # 检查档案在不在
-	print('yeah! 找到档案了')
-	with open('products.csv', 'r', encoding='utf-8') as f:
+def read_file(filename):
+	products = []
+	with open(filename, 'r', encoding='utf-8') as f:
 		for line in f:
 			if '商品, 价格, 日期' in line:
 				continue
-			s = line.strip().split(',')
-			products.append([s[0], s[1], s[2]])
+		name, price, riqi = line.strip().split(',')
+		products.append([name, price, riqi])
+	return products
+# 用户写入采购数据
+def user_input(products):
+	while True:
+		name = input('名称：')
+		if name == 'q':
+			break
+		price = input('价格：')
+		riqi = input('日期')
+		products.append([name, price, riqi]) 
 	print(products)
-else:
-	print('找不到档案...')
-# 写入采购数据
-while True:
-	name = input('名称：')
-	if name == 'q':
-		break
-	price = input('价格：')
-	data = input('日期')
-	#p = []
-	#p.append(name)
-	#p.append(price)
-	#p = [name, price] 
-	#products.append(product)
-	products.append([name, price, data]) # 11,12 == 13
-print(products)
-# 印出购买记录
-for p in products:
-	print(p[0], '的价格：', p[1], ',', '日期/:', p[2])
-# 写入 档案
-with open('products.csv', 'w', encoding='utf-8') as f:
-	f.write('商品, 价格, 日期\n')
+	return products
+# 印出所有购买记录
+def print_products(products):
 	for p in products:
-		f.write(p[0] + ',' + p[1] + ',' + p[2] + '\n')
+		print(p[0], '的价格是：', p[1], ',', '采购日期:', p[2])
+# 写入 档案
+def write_file(filename, products):
+	with open(filename, 'w', encoding='utf-8') as f:
+		f.write('商品, 价格, 日期\n')
+		for p in products:
+			f.write(p[0] + ',' + p[1] + ',' + p[2] + '\n')
 
 
+def main():
+	filename = 'products.csv'
+	if os.path.isfile(filename):  # 检查档案在不在
+		print('yeah! 找到档案了')
+		products = read_file(filename)	 
+	else:
+		print('找不到档案...')
+	products = user_input(products)
+	print_products(products)
+	write_file('products.csv', products)
 
 
-
-
-
-
-
+main()
